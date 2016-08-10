23eb82a1894de8e631e041b17c3726624b07365a
Xuwp <cmtj199337@163.com>
2016-08-10 11:21:01 +0800

    这次的更新还有点问题，但是还是先更新上去把

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GalleryByReactApp from './components/GalleryByReactApp';
 
-// Render the main component into the dom
+
 ReactDOM.render(<GalleryByReactApp />, document.getElementById('content'));

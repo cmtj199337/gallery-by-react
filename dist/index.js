227fed25f44e6a46cf92b173b1c86bc8bd2c3daa
Xuwp <cmtj199337@163.com>
2016-08-30 18:13:20 +0800

    add index.html

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GalleryByReactApp from './components/GalleryByReactApp';
+
+
+ReactDOM.render(<GalleryByReactApp />, document.getElementById('content'));
